a999b0a4b74df85be96dfc02233ed13cb9b37006
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-07-21 06:36:39 +0300

    simple helper fuction

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,5 +5,5 @@ my_tuple = ()
 
 
 def index_range(page, page_size):
-    my_tuple = (page_size * page - page_size, page_size * page)
+    my_tuple = ((page - 1) * page_size, page_size * page)
     return my_tuple
