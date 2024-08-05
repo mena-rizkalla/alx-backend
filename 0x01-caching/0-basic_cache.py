742f92facfae8009c5d1215333c01689a5ada57f
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-08-05 14:38:21 +0300

    basic cache implementation

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+   BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class inherits from BaseCaching class
+    """
+
+    def __init__(self):
+        """ Initialize BasicCache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
