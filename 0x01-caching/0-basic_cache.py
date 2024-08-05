c970476696afa779fe738df326e541328a7b91b0
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-08-05 14:45:55 +0300

    document

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,10 +16,14 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        """ Assign data to cache_data dictionary
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ implement get function
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
